75f4a5fa05c8249ddaeb8f33acb973a6bbfe1814
rinsheed <rinsheedhussain@outlook.com>
2020-09-09 12:57:35 +0530

    created promotion model

---
+++ PromotionEngine/PromotionModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace PromotionEngine
+{
+    public class PromotionModel
+    {
+        public List<ItemModel> Items { get; set; }
+        public int Value { get; set; }
+    }
+    public class ItemModel
+    {
+        public string ItemId { get; set; }
+        public int Count { get; set; }
+    }
+}
